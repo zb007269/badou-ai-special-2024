679af95e95d7b0bf20a749a5fbd9b41407e5c291
zb007269 <76904435+zb007269@users.noreply.github.com>
2024-04-04 17:29:38 +0800

    Add files via upload

---
+++ 张博-299/高斯噪声1.py
@@ -0,0 +1,26 @@
+import numpy as np
+from numpy import shape
+import random
+import cv2
+def GuanssiansNoise(src,means,sigma,percetage):
+    NoiseImg =src
+    NoiseNum = int(percetage*src.shape[0]*src.shape[1])
+    for i in range(NoiseNum):
+        randX = random.randint(0,src.shape[0]-1)
+        randY= random.randint(0, src.shape[1] - 1)
+        NoiseImg[randX,randY]=NoiseImg[randX,randY]+random.gauss(means,sigma)
+        if NoiseImg[randX,randY]<0:
+            NoiseImg[randX, randY]=0
+        elif NoiseImg[randX,randY]>255:
+            NoiseImg[randX, randY]=255
+    return NoiseImg
+img = cv2.imread('lenna.png',0)
+img1 = GuanssiansNoise(img,2,4,0.8)
+img = cv2.imread('lenna.png')
+img2 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow('b',img1)
+cv2.imshow('source',img1)
+cv2.waitKey()
+
+
+

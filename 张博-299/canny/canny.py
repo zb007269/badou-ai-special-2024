1789c6463b06235089a10435cc28f3843e4ebdb7
zb007269 <76904435+zb007269@users.noreply.github.com>
2024-04-07 11:31:34 +0800

    Add files via upload

---
+++ 张博-299/canny/canny.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+img = cv2.imread('lenna.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow('canny',cv2.Canny(gray,200,300))
+cv2.waitKey()
+cv2.destroyAllWindows()

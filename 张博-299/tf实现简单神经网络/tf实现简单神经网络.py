99e6f9e45bb8083e1cb8b8873e91f40d0fdda3a0
zb007269 <76904435+zb007269@users.noreply.github.com>
2024-04-14 18:56:37 +0800

    Add files via upload

---
+++ 张博-299/tf实现简单神经网络/tf实现简单神经网络.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+import  matplotlib .pyplot as plt
+x_data=np.linspace(-0.5,0.5,200)[:,np.newaxis]
+noise=np.random.normal(0,0.02,x_data.shape)
+y_data=np.square(x_data)+noise
+x=tf.placeholder(tf.float32,[None,1])
+y=tf.placeholder(tf.float32,[None,1])
+Weights_L1 =tf.Variable(tf.random_normal([1,10]))
+biases_L1 = tf.Variable(tf.zeros([1,10]))
+Wx_plus_b_L1 = tf.matmul(x,Weights_L1)+biases_L1
+L1 = tf.nn.tanh(Wx_plus_b_L1)
+Weights_L2 =tf.Variable(tf.random_normal([10,1]))
+biases_L2 = tf.Variable(tf.zeros([1,1]))
+Wx_plus_b_L2 = tf.matmul(L1,Weights_L2)+biases_L2
+predition = tf.nn.tanh(Wx_plus_b_L2)
+loss = tf.reduce_mean(tf.square(y-predition))
+train_step= tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(2000):
+        sess.run(train_step,feed_dict={x:x_data,y:y_data})
+    predition_value=sess.run(predition,feed_dict={x:x_data})
+plt.figure()
+plt.scatter(x_data,y_data)
+plt.plot(x_data,predition_value,'r-',lw=5)
+plt.show()
+
